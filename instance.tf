47bfedf41ccd6754eb1302f9223310f02132e612
bunny07-tech <42772445+bunny07-tech@users.noreply.github.com>
2020-06-15 04:16:10 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,6 +5,21 @@ resource "aws_instance" "task1" {
   key_name      = "petro-key"
   security_groups = [ "task1-securitygroup" ]
 
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = tls_private_key.task_keypair.private_key_pem
+    host     = aws_instance.task_instance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd git -y",
+      "sudo systemctl restart httpd",
+      "sudo systemctl enable httpd",
+    ]
+  }
  tags = {
     Name = "petroterraformOS"
   }
