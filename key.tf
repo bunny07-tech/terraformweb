6cbcf7718bf78aca8770574c92cec9c062c8d73f
bunny07-tech <42772445+bunny07-tech@users.noreply.github.com>
2020-06-15 04:37:20 +0530

    Update key.tf

--- key.tf
+++ key.tf
@@ -1,4 +1,16 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "petro-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAu7lJFobQu2VYDsBPXrdFdQwPV8YrukMhBPq0qA7cn2vebG8huXTGXcJXA4xDkxFj8wLnMJQZBXpaWRjUaxYQLSOn2R0w+nAoPwPybijeReuuJ/JhPx3+dvdVW/SzTojdwmZQFRT4/7jA4vrTLeU2Wywqe+9gCrZqwWI9TcQquO0r5bbtXXn0cBv6lcPeajOncQFleEBvWV5h7z1zMZEZm7HG5S3TDTE5w//ND+Nm9TDrEwFVk4GsePQn1ahA+HYAwYuGnh0oDmK06R7Gq9Ij+7uBi/kUeuMOCyo1xkM0IB3mqTU+9bsJATq0197ncymQpx1a3hghRr7KakIILaYosw== rsa-key-20200611"
+resource "tls_private_key" "t1_key" {             //key generation 
+  algorithm = "RSA"
+}
+
+
+resource "local_file" "t1_privatekey" {
+    content     = tls_private_key.t1_key.private_key_pem
+    filename = "petro-key.pem"
+    file_permission = 0400                             
+}
+
+
+resource "aws_key_pair" "t1_key"{                 
+	key_name= "petro-key"
+	public_key = tls_private_key.t1_key.public_key_openssh
 }
